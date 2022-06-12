9d5a4b12ffeb4850003383241a859aa2babbfd81
wangjianwen <wang.jianwen@towngas.com.cn>
2022-06-12 19:49:17 +0800

    bug fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,7 +9,7 @@
     <!-- Bootstrap 3.3.5 -->
     <link rel="stylesheet" href="{{ admin_asset("/vendor/laravel-admin/AdminLTE/bootstrap/css/bootstrap.min.css") }}">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{ admin_asset("/vefiuckndor/laravel-admin/font-awesome/css/font-awesome.min.css") }}">
+    <link rel="stylesheet" href="{{ admin_asset("/vendor/laravel-admin/font-awesome/css/font-awesome.min.css") }}">
     <!-- Theme style -->
     <link rel="stylesheet" href="{{ admin_asset("/vendor/laravel-admin/AdminLTE/dist/css/AdminLTE.min.css") }}">
     <!-- iCheck -->
@@ -52,7 +52,7 @@
             </div>
             <div class="wthree-field">
                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                <button type="submit" class="btn">登录</button>
+                <button type="submit" class="btn">登&nbsp;&nbsp;录</button>
             </div>
 
         </form>
